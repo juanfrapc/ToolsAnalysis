02e60e5c70c89be00e61976db0112ef44b30a09b
juanfrapc <juanfrapc@gmail.com>
2018-01-16 13:52:48 +0000

    Bam parsers implementation init

---
+++ src/parse/BamParser.java
@@ -0,0 +1,21 @@
+package parse;
+
+import Model.Alignment;
+import Model.AlignmentFactory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
+public class BamParser {
+
+    public Stream<Alignment> parse(File file) throws IOException {
+        final ProcessBuilder pb = new ProcessBuilder("samtools", "view", file.getAbsolutePath());
+        final Process process = pb.start();
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        return reader.lines().map(AlignmentFactory::createAligment);
+    }
+
+}
