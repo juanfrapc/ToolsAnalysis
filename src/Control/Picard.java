a674f8de4d6342ba2cc04b1d5228af49659e2b58
juanfrapc <juanfrapc@gmail.com>
2018-04-19 12:23:14 +0100

    Adding Picard.

---
+++ src/Control/Picard.java
@@ -0,0 +1,21 @@
+package Control;
+
+import java.io.IOException;
+
+public class Picard {
+
+    public static String picard = "/home/juanfrapc/Público/picard.jar";
+
+    public static Process markDuplicates (String unmarked, String marked, String metrics) throws IOException {
+        ProcessBuilder pb = new ProcessBuilder("java", "-jar", "MarkDuplicates",
+                "INPUT="+ unmarked,
+                "OUTPUT="+ marked,
+                "MAX_RECORDS_IN_RAM=2000000",
+                "VALIDATION_STRINGENCY=SILENT",
+                "ASSUME_SORT_ORDER=coordinate",
+                "METRICS_FILE"+metrics);
+        pb = pb.redirectError(ProcessBuilder.Redirect.INHERIT);
+        return pb.start();
+    }
+
+}
